44cf24a89434042469deb8c5c6976eed6d2d7c37
Nelson Avila <nelson.avila@redtecnologica.org>
2025-10-05 11:53:39 -0300

    feat: creation of the main pages

---
+++ src/pages/PokemonList.tsx
@@ -0,0 +1,3 @@
+export default function PokemonList() {
+    return <h1>Pokemon List Page</h1>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@public': path.resolve(__dirname, './public'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@store': path.resolve(__dirname, './src/store'),
+    },
+  },
 })
