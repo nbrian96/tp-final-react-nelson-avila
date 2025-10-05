c1239403bf61406790d6315f7128f0aebf680333
Nelson Avila <nelson.avila@redtecnologica.org>
2025-10-05 13:00:43 -0300

    feat: navbar creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+
 import Home from '@pages/Home'
 import PokemonList from '@pages/PokemonList'
 import PokemonDetails from '@pages/PokemonDetails'
-import './App.css'
+import Navbar from '@components/Navbar'
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pokemon" element={<PokemonList />} />
-        <Route path="/pokemon/:id" element={<PokemonDetails />} />
-        <Route path='*' element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Navbar />
+      <Box component="main" sx={{ flexGrow: 1 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon" element={<PokemonList />} />
+          <Route path="/pokemon/:id" element={<PokemonDetails />} />
+          <Route path='*' element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Box>
+    </Box>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
 import reactLogo from '@assets/react.svg'
-import viteLogo from '@public/vite.svg'
+import './Home.css'
 
 export default function Home() {
     const [count, setCount] = useState(0)
 
-    return <>
+    return <div className="home-container">
         <h1>Home Page</h1>
         <div>
-            <a href="https://vite.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
             <a href="https://react.dev" target="_blank">
                 <img src={reactLogo} className="logo react" alt="React logo" />
             </a>
@@ -27,5 +24,5 @@ export default function Home() {
         <p className="read-the-docs">
             Click on the Vite and React logos to learn more
         </p>
-    </>;
+    </div>;
 }
